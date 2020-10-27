d600f6cb95526fbda629bbe0992ab21e16d12087
Zarema <zaremakayasova@gmail.com>
2020-10-27 14:51:01 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let input = prompt("What would you like to do?");
+const todos = [];
+
+while (input !== "quit" && input !== "q") {
+    if (input === "list") {
+        console.log("*************");
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("*************`");
+    } else if (input === "new") {
+        const newTodo = prompt("what is the new todo?");
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`);
+    } else if (input === "delete") {
+        const index = parseInt(prompt("Ok, enter an index to delete"));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`OK, DELETED ${deleted[0]}`);
+        } else {
+            console.log("Unknown index");
+        }
+    }
+    input = prompt("What would you like to do?");
+}
+
+console.log("YOU QUIT THE APP!");
